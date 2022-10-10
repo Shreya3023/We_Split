1a281015ef2fae3a724b03926d34cbfead772a61
SHREYA PRASAD <prasadshreya2303@gmail.com>
2022-10-10 20:26:36 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Shreya Prasad on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
