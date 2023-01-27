aee25cb53eb010cbdd64cd93c049610e28c9abc4
SHREYA PRASAD <prasadshreya2303@gmail.com>
2023-01-27 14:47:25 +0530

    App for splitting the bill amount

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,17 +8,62 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    @FocusState private var amountIsFocused : Bool
+    let tipPercentges = [10,20,30,25,0]
+    var totalPerPerson : Double{
+       //calculate total per person
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        let tipValue = checkAmount /  100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        let amountPerPerson = grandTotal + peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    Picker("Number of people",selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0)people")
+                        }
+                    }
+                }
+                Section{
+                    Picker("Tip Percentage", selection: $tipPercentage){
+                        ForEach(tipPercentges, id:\.self){
+                            Text($0,format: .percent)
+                        }
+                    }.pickerStyle(.segmented)
+                } header: {
+                    Text("How much tip do you want to leave ?")
+                }
+                Section{
+                    Text(totalPerPerson, format: .currency(code:Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit 💸")
+            .toolbar{
+                ToolbarItemGroup(placement:.keyboard ){
+                    Spacer()
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+                
+            }
+            
         }
-        .padding()
+        
     }
 }
-
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
